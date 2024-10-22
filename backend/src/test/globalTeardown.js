6673d16dff20876882ea22cbefecaa9bc6f982ce
Unwana Udo <unwana.udo@flo-dojo.com>
2024-10-22 12:37:15 -0600

    Deleted tests

--- backend/src/test/globalTeardown.js
+++
@@ -1,6 +0,0 @@
-export default async function globalTeardown() {
-  if (global.__MONGOINSTANCE) {
-    await global.__MONGOINSTANCE.stop()
-    console.log('In-memory MongoDB server stopped')
-  }
-}
